ae07b06d73bd061333b96cb8a3334bc9604ff8c7
Vignesh G <71144564+vignesh2311@users.noreply.github.com>
2021-08-26 18:59:37 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
 import socket
-SERVER_IP ="192.168.1.15"
+SERVER_IP ="YOUR IP"
 SERVER_PORT =8080
-size =1024
 if __name__ == "__main__":
     sock =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     address =(SERVER_IP,SERVER_PORT)
     sock.connect(address)
-    rev =sock.recv(size)
+    rev =sock.recv(1024)
     print(rev.decode())
     sock.close()
 
